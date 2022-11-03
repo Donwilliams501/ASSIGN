ff512b01d98e0d21e1c3ca7a2042c174167e2daf
Anthony Anso <115613683+Quater-guy@users.noreply.github.com>
2022-11-03 20:41:42 +0100

    Update ass7.py

--- Anso/ass7.py
+++ Anso/ass7.py
@@ -1,3 +1,6 @@
+# #  Write code that will display the joke “What do you call a bear with no
+#       teeth?” and on the next line display the answer “A gummy bear!” Try to
+#      create it using only one line of code.
 name = input("What is your name? ")
 age = int(input("How old are you? "))
 newAge = age + 1
