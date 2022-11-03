d87f412111a3ec652429ccd46c75781a1e3333e8
Anthony Anso <115613683+Quater-guy@users.noreply.github.com>
2022-11-03 20:40:46 +0100

    Update ass6.py

--- Anso/ass6.py
+++ Anso/ass6.py
@@ -1,3 +1,6 @@
+# #  Write code that will display the joke “What do you call a bear with no
+#       teeth?” and on the next line display the answer “A gummy bear!” Try to
+#      create it using only one line of code.
 starNum =int(input("Enter the number of slices of pizza you started with: "))
 endNun =int(input("How many slices have you eaten? "))
 slicesleft = startNum - endNum
